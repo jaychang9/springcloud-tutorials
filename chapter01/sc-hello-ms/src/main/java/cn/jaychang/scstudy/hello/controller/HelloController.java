568d80f4ee7fecb84b103524c2b2ed12594a3247
Jay Chang <jaychang1987@gmail.com>
2018-09-19 23:31:26 +0800

    first init

---
+++ chapter01/sc-hello-ms/src/main/java/cn/jaychang/scstudy/hello/controller/HelloController.java
@@ -0,0 +1,20 @@
+package cn.jaychang.scstudy.hello.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Jay Chang
+ * @date 2018/9/19
+ */
+@RestController
+public class HelloController {
+    @Value("${server.port:8011}")
+    private String serverPort;
+    @GetMapping("hello")
+    public String hello(String name){
+        return "Hello,I am "+name+",I am listening on port "+serverPort;
+    }
+
+}
---
+++ chapter01/sc-registry-server/src/main/java/cn/jaychang/scstudy/registryserver/RegistryEurekaServerApplication.java
@@ -0,0 +1,17 @@
+package cn.jaychang.scstudy.registryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Jay Chang
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class RegistryEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryEurekaServerApplication.class, args);
+	}
+}
