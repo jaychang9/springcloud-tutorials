05d9b63430c10c80cef7ed0d31fe55ec1ed86396
张洁 <zhangjie@zcckj.com>
2018-09-20 12:36:52 +0800

    chapter03之feign

--- chapter03/sc-feign-ms/src/main/java/cn/jaychang/scstudy/scfeignms/controller/HelloController.java
+++ chapter03/sc-feign-ms/src/main/java/cn/jaychang/scstudy/scfeignms/controller/HelloController.java
@@ -2,7 +2,6 @@ package cn.jaychang.scstudy.scfeignms.controller;
 
 import cn.jaychang.scstudy.scfeignms.feign.HelloFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
