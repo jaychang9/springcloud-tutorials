56022728907a27166a8cd86ec70290e900c234cf
张洁 <zhangjie@zcckj.com>
2018-09-27 18:18:45 +0800

    chapter07配置中心服务注册到注册中心

--- chapter06/README.md
+++ chapter06/README.md
@@ -1,1 +1,3 @@
 # 第六章 配置中心
+
+需要注意的是sc-configclient
---
+++ chapter07/sc-config-server-v2-ms/src/main/java/cn/jaychang/scstudy/scconfigserverv2ms/ConfigServerV2MsApplication.java
@@ -0,0 +1,17 @@
+package cn.jaychang.scstudy.scconfigserverv2ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@EnableEurekaClient
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerV2MsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerV2MsApplication.class, args);
+	}
+}
