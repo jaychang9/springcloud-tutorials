5e7880a493929fb9f3a8998218a12f6d67c0929a
张洁 <zhangjie@zcckj.com>
2018-09-28 13:48:11 +0800

    修改foo的配置测试/bus-refresh功能

--- chapter08/sc-config-client-v3-ms/src/main/java/cn/jaychang/scstudy/scconfigclientms/ConfigClientV3MsApplication.java
+++ chapter08/sc-config-client-v3-ms/src/main/java/cn/jaychang/scstudy/scconfigclientms/ConfigClientV3MsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableEurekaClient
 @EnableDiscoveryClient
 @RestController
+@RefreshScope
 public class ConfigClientV3MsApplication {
 	@Value("${foo}")
 	String foo;
