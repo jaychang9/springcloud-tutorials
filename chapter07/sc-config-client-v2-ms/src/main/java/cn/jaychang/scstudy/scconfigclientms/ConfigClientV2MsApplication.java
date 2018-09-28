78ca89b99d0bb75677fdecf2c27df522f5505ae8
张洁 <zhangjie@zcckj.com>
2018-09-28 12:52:51 +0800

    读取配置中心服务的配置（配置中心地址从eureka取，而不是直接写配置中心的地址）

--- chapter07/sc-config-client-v2-ms/src/main/java/cn/jaychang/scstudy/scconfigclientms/ConfigClientV2MsApplication.java
+++ chapter07/sc-config-client-v2-ms/src/main/java/cn/jaychang/scstudy/scconfigclientms/ConfigClientV2MsApplication.java
@@ -3,11 +3,15 @@ package cn.jaychang.scstudy.scconfigclientms;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RestController
 public class ConfigClientV2MsApplication {
 	@Value("${foo}")
 	String foo;
